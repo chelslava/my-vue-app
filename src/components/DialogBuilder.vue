8a9aeb2c3da46c974ba518caf8cabcc3bfe3ad74
intereskrabote <chelslava@gmail.com>
2024-05-23 09:31:57 +0300

    Обновлен блок IF ELSE

--- src/components/DialogBuilder.vue
+++ src/components/DialogBuilder.vue
@@ -1,49 +1,63 @@
 <template>
   <div class="w-3/4 p-4">
     <p class="text-2xl mb-4">Конструктор диалогов</p>
-    <draggable v-model="blocks" item-key="id" group="blocks">
+    <draggable v-model="blocks" item-key="id" group="blocks" :clone="cloneBlock">
       <template #item="{ element }">
         <div v-if="element.type !== 'ifelse'" class="draggable p-4 bg-gray-100 mb-2">
           {{ element.content }}
           <button @click="removeBlock(element.id)" class="ml-4 text-red-500">Удалить</button>
         </div>
         <div v-else class="draggable ifelse p-4 bg-gray-100 mb-2">
-          {{ element.content }}
           <div class="ifelse-section">
-            <p class="ifelse-title">IF:</p>
-            <div class="drop-zone-vertical">
-              <draggable v-model="element.ifBlocks" item-key="id" group="nestedBlocks" :clone="cloneBlock">
-                <template #item="{ element: ifBlock, index }">
-                  <div class="draggable p-4 bg-gray-200 mb-2">
-                    {{ ifBlock.content }}
-                    <button @click="removeNestedBlock(element, 'ifBlocks', index)" class="ml-4 text-red-500">Удалить</button>
-                  </div>
-                </template>
-                <template #footer>
-                  <div class="drop-zone p-4 bg-gray-50 mb-2 border border-dashed border-gray-400">
-                    Перетащите сюда блок для секции IF
-                  </div>
-                </template>
-              </draggable>
-            </div>
+            <p class="ifelse-title">Если:</p>
+            <!-- Место для перетаскивания блоков IF -->
+            <draggable v-model="element.ifBlocks" item-key="id" group="blocks" :clone="cloneBlock">
+              <template #item="{ element: ifBlock }">
+                <div class="draggable p-4 bg-gray-200 mb-2">
+                  {{ ifBlock.content }}
+                  <button @click="removeNestedBlock(element, 'ifBlocks', ifBlock.id)" class="ml-4 text-red-500">Удалить</button>
+                </div>
+              </template>
+              <template #footer>
+                <div class="drop-zone full-width p-4 bg-gray-50 mb-2 border border-dashed border-gray-400">
+                  Перетащите сюда блок для секции "Если"
+                </div>
+              </template>
+            </draggable>
           </div>
           <div class="ifelse-section">
-            <p class="ifelse-title">ELSE:</p>
-            <div class="drop-zone-vertical">
-              <draggable v-model="element.elseBlocks" item-key="id" group="nestedBlocks" :clone="cloneBlock">
-                <template #item="{ element: elseBlock, index }">
-                  <div class="draggable p-4 bg-gray-200 mb-2">
-                    {{ elseBlock.content }}
-                    <button @click="removeNestedBlock(element, 'elseBlocks', index)" class="ml-4 text-red-500">Удалить</button>
-                  </div>
-                </template>
-                <template #footer>
-                  <div class="drop-zone p-4 bg-gray-50 mb-2 border border-dashed border-gray-400">
-                    Перетащите сюда блок для секции ELSE
-                  </div>
-                </template>
-              </draggable>
-            </div>
+            <p class="ifelse-title">ТО:</p>
+            <!-- Место для перетаскивания блоков THEN -->
+            <draggable v-model="element.thenBlocks" item-key="id" group="blocks" :clone="cloneBlock">
+              <template #item="{ element: thenBlock }">
+                <div class="draggable p-4 bg-gray-200 mb-2">
+                  {{ thenBlock.content }}
+                  <button @click="removeNestedBlock(element, 'thenBlocks', thenBlock.id)" class="ml-4 text-red-500">Удалить</button>
+                </div>
+              </template>
+              <template #footer>
+                <div class="drop-zone full-width p-4 bg-gray-50 mb-2 border border-dashed border-gray-400">
+                  Перетащите сюда блок для секции "ТО"
+                </div>
+              </template>
+            </draggable>
+          </div>
+          <div class="ifelse-section">
+            <p class="ifelse-title">ИНАЧЕ:</p>
+            <!-- Место для перетаскивания блоков ELSE -->
+            <draggable v-model="element.elseBlocks" item-key="id" group="blocks" :clone="cloneBlock">
+              <template #item="{ element: elseBlock }">
+                <div class="draggable p-4 bg-gray-200 mb-2">
+                  {{ elseBlock.content }}
+                  <button @click="removeNestedBlock(element, 'elseBlocks', elseBlock.id)" class="ml-4 text-red-500">Удалить</button>
+                </div>
+              </template>
+              <template #footer>
+                <div class="drop-zone full-width p-4 bg-gray-50 mb-2 border border-dashed border-gray-400">
+                  Перетащите сюда блок для секции "ИНАЧЕ"
+                </div>
+              </template>
+            </draggable>
           </div>
           <button @click="removeBlock(element.id)" class="ml-4 text-red-500">Удалить IF ELSE</button>
         </div>
@@ -64,6 +78,7 @@ const addBlock = (block) => {
     ...block,
     id: Date.now(),
     ifBlocks: block.type === 'ifelse' ? [] : undefined,
+    thenBlocks: block.type === 'ifelse' ? [] : undefined,
     elseBlocks: block.type === 'ifelse' ? [] : undefined
   };
   blocks.value.push(newBlock);
@@ -73,8 +88,8 @@ const removeBlock = (id) => {
   blocks.value = blocks.value.filter(block => block.id !== id);
 };
 
-const removeNestedBlock = (parentBlock, key, index) => {
-  parentBlock[key].splice(index, 1);
+const removeNestedBlock = (parentBlock, key, id) => {
+  parentBlock[key] = parentBlock[key].filter(block => block.id !== id);
 };
 
 const cloneBlock = (block) => {
@@ -109,27 +124,29 @@ defineExpose({
 
 .ifelse {
   width: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
 .ifelse-section {
   margin-bottom: 1rem;
+  width: 100%;
 }
 
 .ifelse-title {
   font-weight: bold;
-}
-
-.drop-zone-vertical {
-  background-color: #f8f8f8;
-  border-radius: 5px;
+  text-align: left;
+  width: 100%;
 }
 
 .drop-zone {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 50px;
   background-color: #f8f8f8;
   border-radius: 5px;
+  margin-bottom: 1rem;
+  width: 100%;
+}
+
+.full-width {
+  width: 100%;
 }
 </style>
